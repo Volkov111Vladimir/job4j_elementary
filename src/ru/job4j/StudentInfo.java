a186b050f13996f349ee31bb7bfde54d958caff4
VladimirVolkov <volfsam.vv@gmail.com>
2020-12-02 15:39:40 +0200

    3.4. Заголовок в файле java [#413099]

--- src/ru/job4j/StudentInfo.java
+++ src/ru/job4j/StudentInfo.java
@@ -1,8 +1,7 @@
-public class StudentInfo {
-    public static void main(String[] args) {
-        System.out.println("1 * 2 = 2");
-        System.out.println("1 * 3 = 3");
-        System.out.println("1 * 9 = 9");
+package ru.job4j;
 
+public class Info {
+    public static void main(String[] args) {
+        System.out.println("19.01.2020");
     }
-}}
+}
