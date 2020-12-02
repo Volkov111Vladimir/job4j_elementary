d5c45b46ad0b4056d8a48a4693ffa05497091121
VladimirVolkov <volfsam.vv@gmail.com>
2020-12-02 14:11:56 +0200

    3.3. Разбор первой программы. [#413098]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("1 * 2 = 2");
+        System.out.println("1 * 3 = 3");
+        System.out.println("1 * 9 = 9");
+
+    }
+}}
